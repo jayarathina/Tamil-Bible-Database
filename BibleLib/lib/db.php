f0c1be9affbfd4dd1de6a9afdfd2ac238eb45db6
Jayarathina Madharasan. Y <jayarathina@users.noreply.github.com>
2019-01-08 00:46:08 +0530

    Update db.php

--- BibleLib/lib/db.php
+++ BibleLib/lib/db.php
@@ -1,17 +1,17 @@
 <?php
+use Medoo\Medoo;
 class db {
 	public $database;
-
 	function __construct() {
 		try {
-			$this->database = new medoo ( array(
+			$this->database = new Medoo ( [ 
 					'database_type' => DB_TYPE,
 					'database_name' => DB_NAME,
 					'server' => DB_HOST,
 					'username' => DB_USER,
 					'password' => DB_PASSWORD,
-					'charset' => DB_CHARSET
-			) );
+					'charset' => DB_CHARSET 
+			] );
 		} catch ( Exception $e ) {
 			if (DEBUG_APP) {
 				echo 'Caught exception: ', $e->getMessage (), '<br/>';
